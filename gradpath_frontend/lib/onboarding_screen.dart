7159e4a7f20fdbbb118a615ceed3463b6232771c
ImmanuellaUmoren <umoren.immanuella@philander.edu>
2026-03-09 21:21:38 -0500

    feat: add Spring 2026 as graduation term option

--- gradpath_frontend/lib/onboarding_screen.dart
+++ gradpath_frontend/lib/onboarding_screen.dart
@@ -84,6 +84,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   ];
 
   final _terms = const [
+    "Spring 2026",
     "Fall 2026",
     "Spring 2027",
     "Fall 2027",
@@ -804,7 +805,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           ),
           const SizedBox(height: 6),
           DropdownButtonFormField<String>(
-            value: _targetTerm,
+            initialValue: _targetTerm,
             decoration: _fieldDecoration("Select a term..."),
             items: _terms
                 .map((t) => DropdownMenuItem(value: t, child: Text(t)))
